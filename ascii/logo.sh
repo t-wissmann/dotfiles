61ed3c364dec6e96f5a446d650a463408f0fc33b
Thorsten Wißmann <edu@thorsten-wissmann.de>
2011-05-08 12:14:06 +0200

    ascii-art: Win7

---
+++ ascii/logo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+red__="\e[0;31m"
+yello="\e[1;32m"
+green="\e[0;33m"
+blue_="\e[1;34m"
+win__="\e[1;37m"
+seven="\e[1;36m"
+subsc="\e[0;35m"
+echo -e " $red__     .-._. $green .-._. $win__ _       ___           __                   $seven _____"
+echo -e " $red__    /    / $green/    / $win__| |     / (_)___  ____/ /___ _      _______ $seven/__  /"
+echo -e " $red__   /.-._/ $green/.-._/  $win__| | /| / / / __ \/ __  / __ \ | /| / / ___/ $seven  / / "
+echo -e " $blue_  .-._. $yello .-._.    $win__| |/ |/ / / / / / /_/ / /_/ / |/ |/ (__  )  $seven / /  "
+echo -e " $blue_ /    / $yello/    /    $win__|__/|__/_/_/ /_/\__,_/\____/|__/|__/____/   $seven/_/   "
+echo -e " $blue_/.-._/ $yello/.-._/     ${subsc}Windows 7 Professional Edition (c) Microsoft 2009 "
+echo -e "\e[0m"
+
+
+
+
+
+ 
