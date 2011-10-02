4c0c35ecc109a6a6f0d4815482a101d67e3f6146
Thorsten Wissmann <re06huxa@stud.informatik.uni-erlangen.de>
2011-10-02 02:26:44 +0200

    hlwm-info fixed printf-space problems

--- ascii/hlwm-info.sh
+++ ascii/hlwm-info.sh
@@ -6,7 +6,7 @@ f="" #just for alignment :D
 
 distro="ArchLinux"
 if [ -f /etc/debian_version ] ; then
-    distro="debian $(cat /etc/debian_version)"
+    distro="Debian $(cat /etc/debian_version)"
 fi
 
 status=
@@ -36,7 +36,7 @@ cat <<EOF
  ${v} x $t╾───╮
 $space$t ├─${n}WM:$v Herbstluftwm$t─┬─${n}version: $v$(herbstluftwm --version|head -1)
 $logo1$t │                  ├─${n}panel:$v dzen2
-$logo2$t ├─${n}OS:$v $(printf %-13s $distro)${t}╰─${n}statusbar: $v$status
+$logo2$t ├─${n}OS:$v $(printf %-13s "$distro")${t}╰─${n}statusbar: $v$status
 $logo3$t ├─${n}Kernel: $v$(uname -sr)
 $logo4 $t├─${n}Shell:$v $SHELL
 $space$t ├─${n}Terminal:$v $terminal$t──${n}Font: $v$font
