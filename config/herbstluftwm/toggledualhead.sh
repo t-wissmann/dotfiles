cf34976c6d611ba22a784ab03e8f3afcd70a96bb
Thorsten Wißmann <edu@thorsten-wissmann.de>
2012-08-24 08:50:17 +0200

    Fix toggledualhead.sh

--- config/herbstluftwm/toggledualhead.sh
+++ config/herbstluftwm/toggledualhead.sh
@@ -14,7 +14,7 @@ fi
 
 resolution=$(
      xwininfo -root |
-        grep -E ' Width: | Height: ' |  # only get width and height
+        grep --binary-files=text -E ' Width: | Height: ' |  # only get width and height
         sort -r |                       # width first
         cut -d' ' -f4 |                 # only print values
         sed '1 s/$/x/' |                # append x to first line
@@ -24,18 +24,13 @@ resolution=$(
 function dualhead() {
     width="$1"
     height="$2"
-    hc focus_monitor 0
-    hc remove_monitor 1
-    hc move_monitor 0 $((width/2))x$((height))+0+0 $pad
-    hc add_monitor $((width/2))x$((height))+$((width/2))+0 "" $pad
+    hc set_monitors $((width/2))x$((height))+{0,$((width/2))}+0
 }
 
 function singlehead() {
     width="$1"
     height="$2"
-    hc focus_monitor 0
-    hc remove_monitor 1
-    hc move_monitor 0 $((width))x$((height))+0+0 $pad
+    hc set_monitors $((width))x$((height))+0+0
 }
 
 cur_monitor_count=$(herbstclient list_monitors | wc -l)
