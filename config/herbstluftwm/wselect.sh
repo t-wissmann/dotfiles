f543f2af3561e4f094a9c0a16f56820e586bec68
Thorsten Wißmann <edu@thorsten-wissmann.de>
2012-08-29 11:05:06 +0200

    new wselect.sh

--- config/herbstluftwm/wselect.sh
+++ config/herbstluftwm/wselect.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 source ~/.bash_settings
-id=$(wmctrl -l | $dmenu_command -l $dmenu_lines ) && xdotool windowactivate ${id%% *}
+
+case "$1" in
+    bring)
+        name=Bring:
+        action() {
+            herbstclient bring "$@"
+            xdotool windowactivate "$@"
+        }
+        ;;
+    select|*)
+        action() { xdotool windowactivate "$@" ; }
+        name=Select:
+        ;;
+esac
+
+id=$(wmctrl -l | $dmenu_command -l $dmenu_lines -p "$name") \
+    && action ${id%% *}
+
