a913ed0d56c21cafc7388bda547cf304c0747be4
Thorsten Wißmann <re06huxa@stud.informatik.uni-erlangen.de>
2012-09-27 00:13:28 +0200

    mehr cip anpassungen

--- config/herbstluftwm/restart.sh
+++ config/herbstluftwm/restart.sh
@@ -3,6 +3,7 @@
 
 STATEFILE=~/.config/herbstluftwm/state
 
-~/dev/c/herbstluftwm/scripts/savestate.sh > "$STATEFILE"
+~/dev/c/herbstluftwm/scripts/savestate.sh > "$STATEFILE" ||
+~/git/herbstluftwm/scripts/savestate.sh > "$STATEFILE"
 herbstclient wmexec
 
