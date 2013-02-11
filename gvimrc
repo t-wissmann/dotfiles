ea1a1948171b9c15d934334ab2b4f6b74335ea62
Thorsten Wißmann <edu@thorsten-wissmann.de>
2013-02-11 17:52:09 +0100

    Better gvim colos

--- gvimrc
+++ gvimrc
@@ -1,10 +1,11 @@
 
 
 "colorscheme wombat
-colorscheme solarized
+let g:solarized_visibility="high"    "default value is normal
 set bg=light
+colorscheme solarized
 " remove all guielements
-set guioptions-=m
+set guioptions+=m
 set guioptions-=T
 set guioptions-=r
 set guioptions-=R
--- setup-vim.sh
+++ setup-vim.sh
@@ -1,16 +1,20 @@
 #!/bin/bash -e
 
-# set up ~/.vim-folder
-mkdir -p ~/.vim/plugin
-
 fetch() {
     if ! [ -f "$1" ] ; then
         mkdir -p "${1%/*}"
         wget -O "$1" "$2"
+        sed -i 's/\r//g' "$1"
     else
         echo "$1 already exists"
     fi
 }
+
+auto-colorscheme() {
+    file="${1##*/}"
+    fetch ~/.vim/colors/"$file" "$1"
+}
+
 
 fetch ~/.vim/plugin/minibufexpl.vim \
     'http://www.vim.org/scripts/download_script.php?src_id=3640'
@@ -19,3 +23,14 @@ fetch ~/.vim/colors/jellybeans.vim \
 
 fetch ~/.vim/colors/solarized.vim \
     'https://github.com/altercation/vim-colors-solarized/raw/master/colors/solarized.vim'
+fetch ~/.vim/autoload/togglebg.vim \
+    'https://github.com/altercation/vim-colors-solarized/raw/master/autoload/togglebg.vim'
+
+
+fetch ~/.vim/colors/molokai.vim \
+    'http://www.vim.org/scripts/download_script.php?src_id=9750'
+
+auto-colorscheme 'http://vimcolorschemetest.googlecode.com/svn/colors/autumnleaf.vim'
+auto-colorscheme 'http://vimcolorschemetest.googlecode.com/svn/colors/kate.vim'
+auto-colorscheme 'http://vimcolorschemetest.googlecode.com/svn/colors/tcsoft.vim'
+
