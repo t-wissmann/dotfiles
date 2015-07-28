bc59db5fea35febdcf0c1ffd3e59c4c0d140bc94
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-07-28 10:12:12 +0200

    Improve rofi-mpd.sh

--- menu/rofi-mpd.sh
+++ menu/rofi-mpd.sh
@@ -7,52 +7,88 @@ switcher="$1"
 shift || true
 args="$*"
 
-case "$switcher" in
-    playlist)
-        if [ -z "$args" ] ; then
-            mpc --format "%position%\t$song_format" playlist \
-                | sed 's,^\([^\t]*\t[^\t]\{40\}\)[^\t]*,\1…,' \
-                | column -s $'\t' -t
+switcher_titles=( )
+switcher_ids=( )
+
+add_switcher() {
+    switcher_titles+=( "$1" )
+    switcher_ids+=( "$2" )
+}
+
+playlist() {
+    print() {
+        mpc --format "%position%\t$song_format" playlist \
+            | sed 's,^\([^\t]*\t[^\t]\{40\}\)[^\t]*,\1…,' \
+            | column -s $'\t' -t
+    }
+    execute() {
+        pos="${*%% *}"
+        mpc -q play "$pos"
+    }
+}
+
+addsong() {
+    print() {
+        mpc --format "$song_format\t%file%" search filename '' \
+            | sed 's,^\([^\t]\{40\}\)[^\t]*,\1…,' \
+            | column -s $'\t' -t
+    }
+    execute() {
+        echo "add $*" >&2
+    }
+}
+
+addalbum() {
+    print() {
+        mpc --format '[%albumartist%|%artist%] — %album%' \
+            search filename '' \
+            | grep -v '^ — ' \
+            | grep -v ' — $' \
+            | uniq
+    }
+    execute() {
+        artist="${args%% — *}"
+        album="${args#* — }"
+        output=$(mpc find albumartist "$artist" album "$album")
+        if [ -n "$output" ] ; then
+            # if output was empty, then $artist was the album artist
+            # if $artist was albumartist, then add by albumartist
+            mpc -q findadd albumartist "$artist" album "$album"
         else
-            pos="${args%% *}"
-            mpc -q play "$pos"
+            # else add using the artist tag
+            mpc -q findadd artist "$artist" album "$album"
         fi
-        ;;
-    addsong)
-        if [ -z "$args" ] ; then
-            mpc --format "$song_format\t%file%" search filename '' \
-                | sed 's,^\([^\t]\{40\}\)[^\t]*,\1…,' \
-                | column -s $'\t' -t
-        else
-            echo "add $args" >&2
-        fi
-        ;;
-    addalbum)
-        if [ -z "$args" ] ; then
-            mpc --format '[%albumartist%|%artist%] — %album%' \
-                search filename '' \
-                | grep -v '^ — ' \
-                | grep -v ' — $' \
-                | uniq
-        else
-            artist="${args%% — *}"
-            album="${args#* — }"
-            output=$(mpc find albumartist "$artist" album "$album")
-            if [ -n "$output" ] ; then
-                # if output was empty, then $artist was the album artist
-                # if $artist was albumartist, then add by albumartist
-                mpc -q findadd albumartist "$artist" album "$album"
+    }
+}
+
+add_switcher playlist   Playlist
+add_switcher addalbum   "Add album"
+add_switcher addsong    "Add song"
+
+# compose switcher specification
+switchers=""
+for i in "${!switcher_ids[@]}" ; do
+    switchers+="${switcher_titles[$i]}:$0 ${switcher_ids[$i]},"
+done
+
+if [ -z "$switcher" ] ; then
+    rofi  -no-levenshtein-sort \
+        -switchers "${switchers%,}" \
+        -show "${switcher_titles[0]}"
+else
+    # check whether given $switcher is a valid switcher id
+    for s in "${switcher_ids[@]}" ; do
+        if [ "$s" = "$switcher" ] ; then
+            # load switcher
+            $s
+            if [ -z "$args" ] ; then
+                # if no args given to switcher the print items
+                print
             else
-                # else add using the artist tag
-                mpc -q findadd artist "$artist" album "$album"
+                # otherwise pass args to switcher
+                execute "$args"
             fi
+            exit 0
         fi
-        ;;
-    "")
-        rofi  -no-levenshtein-sort \
-            -switchers "Playlist:$0 playlist,Add album:$0 addalbum,Add song:$0 addsong" \
-            -mode Playlist,Add\ Album,Add\ song -show Playlist
-        ;;
-esac
-
-
+    done
+fi
