e6dfeaaeaf3cb2880a68cd711e77fd1ecf3a264c
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-08-11 22:21:36 +0200

    Add utf8select.sh and utf8db

---
+++ menu/utf8select.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+utf8db="$HOME/dotfiles/menu/utf8db"
+
+if line=$(grep -v '^#' "$utf8db"|grep -v '^$' \
+          |rofi -dmenu -p 'Zeichen:' -columns 3) ; then
+    #echo you selected "$line"
+    # char is everything after the last space
+    char=${line##* }
+    #echo "you selected $char"
+    #xdotool key "$char"
+    echo -n "$char" |xclip -selection clipboard -i
+    echo -n "$char" |xclip -selection primary -i
+    # it is written to clipboard, now type it
+    xdotool key shift+Insert
+fi
+
+
+
