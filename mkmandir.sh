f253a1eba7dc9ec7e00ab763d802dc0a0ca5299d
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-08-17 17:46:54 +0200

    Add mkmandir.sh utility

---
+++ mkmandir.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+# create man page dir for non-globally installed
+# programes
+
+shopt -s nullglob
+
+# setting up a man-dir in ~/man
+::() {
+    echo -e "\e[0;31m:: \e[0;32m$*\e[0m"
+}
+
+MANDIR=$HOME/man
+for d in "$MANDIR" "$MANDIR"/man{0..8} ; do
+    :: Creating "$d"
+    mkdir -p "$d"
+done
+
+searchdir=(
+    ~/git/herbstluftwm/doc/
+    ~/git/qutebrowser/doc/
+    ~/git/rofi/doc/
+)
+
+for d in "${searchdir[@]}" ; do
+    :: Searching for man pages in "$d"
+    for f in "${d%/}"/*.[0-8] ; do
+        section="${f##*.}"
+        target="${MANDIR}/man$section"
+        :: Linking "${target##${MANDIR%/}/}/ <- ${f#$d}"
+        ln -sf "$f" "$target"
+    done
+done
+
+
+
