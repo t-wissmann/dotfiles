b35119946d8c8040b930adb9d35e12f66dedd214
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-10-21 14:49:11 +0200

    Mutt-mailto: work in progress

---
+++ xdg/mutt-mailto.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+import sys
+import subprocess
+import urllib.parse
+
+# we get as a input something like:
+# user@domain?subject=XYZ&cc=...&body=...
+
+
+o     = urllib.parse.urlparse(sys.argv[1])
+query = urllib.parse.parse_qs(o.query)
+
+proc = [ "mutt" ]
+if 'subject' in query:
+    proc += [ '-s', query['subject'][0] ]
+#proc += [ '-H', '-' ] # read message body from stdin
+proc += [ '--', o.path ]
+
+print (proc)
+mutt = subprocess.Popen(proc) #, stdin=subprocess.PIPE)
+#if 'body' in query:
+#mutt.stdin.write(b'test');
+#mutt.stdin.close()
+#(address,parameters) = sys.argv[1].split('?', 1)
+#
+#param_dict = {}
+#
+#for x in parameters.split('&'):
+#    (name,value) = x.split('=', 
+#    param_dict[
