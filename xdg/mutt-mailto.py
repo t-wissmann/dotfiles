2307a4b2385ce82720c417eb0f174fa310564189
Thorsten Wißmann <edu@thorsten-wissmann.de>
2015-11-29 14:05:14 +0100

    Make it work for mailto: and mailto://

--- xdg/mutt-mailto.py
+++ xdg/mutt-mailto.py
@@ -7,11 +7,20 @@ import tempfile
 import os
 
 # we get as a input something like:
-# user@domain?subject=XYZ&cc=...&body=...
+# mailto: user@domain?subject=XYZ&cc=...&body=...
+# or
+# mailto://user@domain?subject=XYZ&cc=...&body=...
 
 o     = urllib.parse.urlparse(sys.argv[1])
 query = urllib.parse.parse_qs(o.query)
 
+address = o.netloc + o.path # one of them is empty
+
+# >>> from urllib.parse import urlparse
+# >>> urlparse('mailto:bla@blub.com')
+# ParseResult(scheme='mailto', netloc='', path='bla@blub.com', params='', query='', fragment='')
+# >>> urlparse('mailto://bla@blub.com')
+# ParseResult(scheme='mailto', netloc='bla@blub.com', path='', params='', query='', fragment='')
 
 draft = tempfile.NamedTemporaryFile(mode='w',delete=False)
 if 'body' in query:
@@ -24,14 +33,17 @@ proc = [ ]
 proc += [ "mutt" ]
 if 'subject' in query:
     proc += [ '-s', query['subject'][0] ]
+if 'cc' in query:
+    for cc in query['cc']:
+        proc += [ '-c', cc ]
 proc += [ '-H', draft.name ]
-proc += [ '--', o.path ]
+proc += [ '--', address ]
 
-#print (proc)
 mutt = subprocess.Popen(proc) #, stdin=subprocess.PIPE)
 mutt.wait()
 #print(draft.name)
 os.unlink(draft.name)
+#print (proc)
 
 #if 'body' in query:
 #mutt.stdin.write(b'test');
