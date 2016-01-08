c8887d098aec7e2d588921b6a0a7f8a7f1fa0a8c
Thorsten Wißmann <edu@thorsten-wissmann.de>
2016-01-08 18:16:54 +0100

    Finish music-zip.sh

--- utils/music-zip.sh
+++ utils/music-zip.sh
@@ -15,33 +15,40 @@ importfile() {
             outfile="${1%.jpeg}.jpg"
             CMD=( ln -f )
             ;;
-        *.png)
-            outfile="${1%.jpeg}.jpg"
-            CMD=( ln -f )
+        *.png|*.gif|*.bmp)
+            outfile="${1%.*}.jpg"
+            CMD=( convert )
             ;;
         *.flac)
-            outfile="${1%.mp3}.flac"
+            outfile="${1%.*}.mp3"
             CMD=( ~/dotfiles/utils/flac2mp3.sh )
             ;;
-        *.wav|*.mood)
+        *.ogg)
+            # temporarily ignore ogg files
+            return
+            ;;
+        *.wav|*.mood|*.log|*.cue|*.AVI|*.mid|*.md5|*.ffp|*.sh|*.m3u|*.pdf|*.css)
             # silently ignore some file types
             return
             ;;
-        *.*)
-            :: "Ignoring unknown filetype \e[1m*.${1##*.}\e[0m: ${1}"
+        *)
+            :: "\e[1;31mIgnoring unknown filetype \e[1;37m*.${1##*.}\e[1;31m:\e[0m ${1}"
             return
             ;;
     esac
     outfile="$destdir/$outfile"
-    infile="$1"
-    if [ -f "$outfile" -a "$infile" -ot "$outfile" ] ; then
+    infile="$srcdir/$1"
+    if [[ -f "$outfile" && ! "$infile" -nt "$outfile" ]] ; then
         # if outfile exists and infile is not newer,
         # then outfile is up to date
         return
     fi
     #:: "$1 -> $outfile"
-    #:: "${CMD[@]}" "$infile" "$outfile" # || return 1
+    :: "${CMD[@]}" "$infile" "$outfile" # || return 1
+    mkdir -p "${outfile%/*}"
+    "${CMD[@]}" "$infile" "$outfile" || return 1
 }
 export -f importfile
 
-find "$1" -type f -printf '%P\n' | parallel importfile
+
+find "$1" -type f -printf '%P\n' | parallel --halt soon,fail=1 importfile
