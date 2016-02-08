e135deb47f16be6bccfe21bdb43359faf86d3bb2
Thorsten Wißmann <edu@thorsten-wissmann.de>
2016-02-08 17:17:54 +0100

    Successfully fail on an error in flac2mp3.sh

--- emacs
+++ emacs
@@ -1,8 +1,8 @@
  (require 'evil)
  (evil-mode 1)
-; (load-file "/usr/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
-; (add-hook 'coq-mode-hook (lambda () (local-set-key (kbd "C-c RET") 'proof-goto-point)))
-;(custom-set-variables
+(load-file "/usr/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
+(add-hook 'coq-mode-hook (lambda () (local-set-key (kbd "C-c RET") 'proof-goto-point)))
+(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
--- utils/flac2mp3.sh
+++ utils/flac2mp3.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+set -o pipefail
+set -e
+
 copytags() {
+  set -o pipefail
+  set -e
   # copies all the tags from the flac file $1 to the mp3 file $2
   eval "$(
      metaflac "$1" --no-utf8-convert \
@@ -13,18 +18,25 @@ copytags() {
                    --show-tag=DATE |
                    sed "s,[\"\'],\\\\&,g" |
                    sed "s,=\(.*\),=$\'\\1\'," |
-                   iconv -f UTF-8 -t ISO-8859-15 |
+                   sed "s,—,-,g" |
+                   iconv -c -f UTF-8 -t ISO-8859-1 |
                    sed 's,^[^=]*,\U&,' # ensure upper case vars
-    )"
+    )" || return 1
   #flac -c -d "$f" | lame -m j -q 0 --vbr-new -V 0 -s 44.1 - "$outf"
   id3v2 -t "$TITLE" -T "${TRACKNUMBER:-0}" -a "$ARTIST" -A "$ALBUM" \
         -y "$DATE" -g "${GENRE:-12}" --TPOS "$DISC" "$2"
 }
 
+rm_and_fail() {
+    #echo "Removing $1 again" >&2
+    rm "$1"
+    exit 1
+}
+
 convlocal() {
     ffmpeg -y -loglevel warning -i "$1" -ab 192k -ac 2 -ar 48000 "$2" \
         && copytags "$1" "$2" \
-        || rm "$2"
+        || rm_and_fail "$2"
 }
 
 HOST=faui00u.cs.fau.de
