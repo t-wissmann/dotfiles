53c2382729251cd04fd8f791da4ac76c84928856
Thorsten Wißmann <edu@thorsten-wissmann.de>
2016-02-20 00:47:30 +0100

    Another comment

--- utils/folderplaylist.sh
+++ utils/folderplaylist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# find -mindepth 1 -type d  | while read -r d; do pushd "$d" ; [ -f play.m3u ] && continue ; echo "$d" ; ~/dotfiles/utils/folderplaylist.sh > play.m3u ; popd; done
+
 find -iname '*.mp3' | while read line ; do
     echo $(id3v2 -l "$line" |grep ^TRCK|sed 's,^.* ,,') "$line"
-done|sort -n|sed 's,[^ ]* \./,,'
+done|sort -n|sed 's,[^ ]* \./,,' | sed 's,$,\r,'
 
