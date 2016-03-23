a0a041ad3115c028a6983cc0958029d76cd6eaee
Thorsten Wißmann <edu@thorsten-wissmann.de>
2016-03-23 19:56:31 +0100

    More

--- utils/folderplaylist.sh
+++ utils/folderplaylist.sh
@@ -2,7 +2,9 @@
 
 # find -mindepth 1 -type d  | while read -r d; do pushd "$d" ; [ -f play.m3u ] && continue ; echo "$d" ; ~/dotfiles/utils/folderplaylist.sh > play.m3u ; popd; done
 
+# or for named playlists:
+# find -mindepth 1 -type d  | while read -r d; do pushd "$d" ;  x="${d#./}" ; echo "${x//\// }" ; ~/dotfiles/utils/folderplaylist.sh > "${x//\// }".m3u ; popd; done
 find -iname '*.mp3' | while read line ; do
     echo $(id3v2 -l "$line" |grep ^TRCK|sed 's,^.* ,,') "$line"
-done|sort -n|sed 's,[^ ]* \./,,' | sed 's,$,\r,'
+done|sort -n|sed 's,[^ ]* \(\./\)*,,' | sed 's,$,\r,'
 
