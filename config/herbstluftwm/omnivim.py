c5980d3fd4bb152a613091d7f0102c37de409448
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-02-14 15:50:29 +0900

    First draft for omnivim

---
+++ config/herbstluftwm/omnivim.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+# vim bindings in any application
+
+exit_omnivim = 'Escape'
+keys = [
+    ('h', 'Left'),
+    ('j', 'Down'),
+    ('k', 'Up'),
+    ('l', 'Right'),
+    ('Shift+4', 'Ctrl+E'),
+    ('0', 'Home'),
+    ('Ctrl+f', 'Next'),
+    ('Ctrl+b', 'Prior'),
+    ('b', 'Ctrl+Left'),
+    ('f', 'Ctrl+Right'),
+    ('w', 'Ctrl+Right'),
+    #('/', 'Ctrl-f'),
+]
+
+bind_cmd = [
+    'herbstclient',
+    'chain',
+]
+bind_cmd_sep = 'NEXTKEY'
+unbind_cmd = [ 'chain' ]
+unbind_cmd_sep = 'UBNEXTKEY'
+for k,m in keys:
+    bind_cmd += [ bind_cmd_sep, 'keybind', k ]
+    k_without_mods = k.split('-')[-1].split('+')[-1]
+    cmd = [ 'xdotool', 'keyup', k_without_mods, 'key', '--clearmodifiers', m ]
+    bind_cmd += [ 'spawn' ] + cmd
+    unbind_cmd += [ unbind_cmd_sep, 'keyunbind', k ]
+
+unbind_cmd += [ unbind_cmd_sep, 'keyunbind', exit_omnivim ]
+
+bind_cmd += [ bind_cmd_sep, 'keybind', exit_omnivim ]
+bind_cmd += unbind_cmd
+
+#print(' '.join(bind_cmd))
+subprocess.call(bind_cmd)
+
