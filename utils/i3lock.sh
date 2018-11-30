6d3e845e7083aad88f1369a48233cbd75bbd952f
Thorsten Wißmann <edu@thorsten-wissmann.de>
2018-11-30 20:43:16 +0100

    Use temp file to use i3lock on large displays

--- utils/i3lock.sh
+++ utils/i3lock.sh
@@ -2,7 +2,13 @@
 
 # from simigern
 geometry=$(xwininfo -root | grep 'geometry' | awk '{print $2}')
-xwd -root \
-    | convert xwd:- -scale 5% -scale "!${geometry}" png:- \
-    | exec i3lock -f -i /dev/stdin
+#xwd -root \
+#    | convert xwd:- -scale 5% -scale '!'"${geometry}" png:- \
+#    | exec i3lock -f -i /dev/stdin
 
+# newer i3lock versions read the image twice, and so we need to do the following:
+img_path="$HOME/.i3lock-blur.png"
+xwd -root | convert xwd:- -scale 5% -scale '!'"${geometry}" png:- > "$img_path"
+chmod 600 "$img_path"
+i3lock -f -i "$img_path"
+rm "$img_path"
