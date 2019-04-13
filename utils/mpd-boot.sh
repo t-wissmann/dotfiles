2277b06c0540db347ebf79addcf5ebe5fda4c520
Thorsten Wißmann <edu@thorsten-wissmann.de>
2019-04-13 11:04:58 +0200

    MPD boot up script

---
+++ utils/mpd-boot.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+ask() {
+    local answer
+    read -s -n 1 -p "$* [yN]" answer
+    if [[ "$answer" == "y" ]] || [[ "$answer" == "Y" ]] ; then
+        echo
+        return 0
+    else
+        echo
+        return 1
+    fi
+}
+
+start_or_restart() {
+    # start the given command
+    # if is already running, restart it
+    cmdname="$1"
+    # 1. check if $cmdname is already running under the present user
+    ps_output=$(ps -U thorsten -x)
+    if line=$(grep " $cmdname$" <<< "$ps_output") ; then
+        echo ":: Process $* already running:"
+        echo "   $line"
+        pid=$(sed 's,^[ ]*\([^ ]\+\) .*$,\1,' <<< "$line")
+        if ask "Kill \"$pid\"?" ; then
+            kill "$pid" # drop prefixing spaces
+        fi
+    fi
+    # 2. run it
+    echo ":: Starting $*"
+    nohup "$@" 2> /dev/null > /dev/null &
+}
+
+start_or_restart mpd
+start_or_restart ~/dotfiles/utils/mpd-volume2pulse.py
+start_or_restart mpdscribble
+start_or_restart cantata
