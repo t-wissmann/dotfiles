670c14e6f822c07f08b87a6e1e2e4d7356abb814
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-05-23 10:22:54 +0200

    Minor improvements

--- etc/luksmount.sh
+++ etc/luksmount.sh
@@ -3,7 +3,7 @@
 # mount a partition that is in a LVM that is in a luks container
 
 ::() {
-    echo "$*" >&2
+    echo ":: $*" >&2
     "$@"
 }
 
@@ -12,7 +12,7 @@ luksDevice=/dev/disk/by-label/sg2020
 # the name used for the decrypted thing, i.e. the lvm physical volume:
 luksName=sg2020-pv
 # the partition in the above lvm to mount:
-partition=/dev/mapper/vgmedia-lvmedia
+partition=/dev/mapper/sg2020--vg-sg2020--backup
 
 if [[ -b /dev/mapper/$luksName ]] ; then
     echo "$luksName is already open"
