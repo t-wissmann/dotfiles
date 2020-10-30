3d524682a74715b0acca608c4dab78a4b4b43a27
Thorsten Wißmann <edu@thorsten-wissmann.de>
2020-10-30 10:44:14 +0100

    Print summary and improve debug output

--- utils/latex-strip-comments.py
+++ utils/latex-strip-comments.py
@@ -5,26 +5,52 @@ import re
 import sys
 import textwrap
 
-def remove_comments(texsrc, comments_re):
+def remove_comments(texsrc, comments_re, summary={}):
     def replacement(matchobj):
+        replacement.count += 1
         return matchobj.group(1)
-    return re.sub(comments_re, replacement, texsrc)
+    replacement.count = 0
+    newsrc = re.sub(comments_re, replacement, texsrc)
+    summary['comments removed'] = replacement.count
+    return newsrc
 
-def remove_macros(texsrc, macro_re):
+def remove_macros(texsrc, macro_re, summary={}):
     newsrc = ''
     pos = 0  # the next position to copy to 'newsrc'
+    macro2count = {}
+    # everything after index i is in line newlineidx2lineno[i]:
+    # (and character 0 is in line '1')
+    newlineidx2lineno = {-1: 1}
+    for idx, ch in enumerate(texsrc):
+        if ch == '\n':
+            assert(texsrc[idx] == '\n')
+            newlineidx2lineno[idx] = len(newlineidx2lineno) + 1
+    def idx2line(idx):
+        """given a position of a character, return its line number"""
+        for i in range(idx, 0, -1):
+            if texsrc[i] == '\n':
+                return newlineidx2lineno[i]
+        return 1
+
     for m in macro_re.finditer(texsrc):
         start = m.start(0)
         end = m.end(0)  # the first character after the match
         if start < pos:
             # skipping nested macro
+            print('Line {}: Skipping »{}« which was nested in another macro that has been already removed'
+                    .format(idx2line(start), m.group(0)),
+                  file=sys.stderr)
             continue
         if end >= len(texsrc) or texsrc[end] != '{':
             expected = '{'
-            instead = texsrc[end:end + 20].replace('\n', '\\n')
-            print(f'Warning: {m.group(0)} is not followed by »{expected}« but by »{instead}« instead.', file=sys.stderr)
-            print(f'         Ignoring this occurrence. (Maybe it is the macro\'s definition)', file=sys.stderr)
+            instead = re.sub('\n.*$', '', texsrc[end:end + 20])
+            line = str(idx2line(start))
+            print(f'Line {line}: {m.group(0)} is not followed by »{expected}« but by »{instead}« instead.', file=sys.stderr)
+            print(f'     {len(line)*" "}  Ignoring this occurrence. (Maybe it is the macro\'s definition)', file=sys.stderr)
         else:
+            # in this branch, we actually do the replacement
+            macro2count[m.group(0)] = macro2count.get(m.group(0), 0) + 1
+
             # copy until 'end + 1'
             newsrc += texsrc[pos:end + 1]
             pos = end + 1
@@ -49,6 +75,8 @@ def remove_macros(texsrc, macro_re):
                         break
     # after all matches being scanned, copy the rest:
     newsrc += texsrc[pos:]
+    for k, v in macro2count.items():
+        summary[f'parameters removed {k}' ] = v
     return newsrc
 
 def main():
@@ -77,6 +105,8 @@ def main():
                         help='Regex for comments: replace the regex by its first group')
     parser.add_argument('--macros', default=r'\\..note(\[[^]]*\])?|\\takeout',
                         help='Regex for the macros whose parameter are to be stripped')
+    parser.add_argument('--summary', action='store_const', const=True, default=False,
+                        help='Print summary to stderr')
     parser.add_argument('file', metavar='FILE', nargs='+',
                         help='tex sources')
     args = parser.parse_args()
@@ -86,9 +116,16 @@ def main():
     for filepath in args.file:
         with open(filepath, 'r') as fh:
             texsrc = fh.read()
-        texsrc = remove_comments(texsrc, comments_re)
-        texsrc = remove_macros(texsrc, macros_re)
+        summary = {}
+        texsrc = remove_comments(texsrc, comments_re, summary=summary)
+        texsrc = remove_macros(texsrc, macros_re, summary=summary)
         print(texsrc)
+        if args.summary:
+            def p(text):
+                print(text, file=sys.stderr)
+            p(f"Summary for »{filepath}«:")
+            for k, v in summary.items():
+                p(f'  {k}: {v}')
 
 
 main()
