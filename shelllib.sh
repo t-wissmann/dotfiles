10020ae0c8b6cf1be42230ddbc4e9b52eb52fe69
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-06-21 08:35:24 +0200

    Add mkusbdrive

--- shelllib.sh
+++ shelllib.sh
@@ -6,3 +6,13 @@
     echo -e "\e[1;33m:: \e[0;32m$*\e[0m" >&2
     "$@"
 }
+
+ask() {
+    echo "==> $1 [y/n]"
+    read -n 1 reply
+    if [[ "${reply^^}" == 'Y' ]] ; then
+        return 0
+    else
+        return 1
+    fi
+}
