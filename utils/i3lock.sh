1f3001cb6a8a0cde92920454f0a5fa3e3b570f22
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-10-11 12:32:07 +0200

    i3lock --nofork

--- utils/i3lock.sh
+++ utils/i3lock.sh
@@ -10,5 +10,5 @@ geometry=$(xwininfo -root | grep 'geometry' | awk '{print $2}')
 img_path="$HOME/.i3lock-blur.png"
 xwd -root | convert xwd:- -scale 5% -scale '!'"${geometry}" png:- > "$img_path"
 chmod 600 "$img_path"
-i3lock -f -i "$img_path"
+i3lock --nofork -f -i "$img_path"
 rm "$img_path"
