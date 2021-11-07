f7a52c9301df29abd48bf4195545055c626eed97
Thorsten Wißmann <edu@thorsten-wissmann.de>
2021-11-07 13:04:30 +0100

    Panel on other monitor

--- config/herbstluftwm/panel.sh
+++ config/herbstluftwm/panel.sh
@@ -2,7 +2,16 @@
 
 
 if [ $HOSTNAME = hoth ] ; then
-    exit 0
+    geo=$(herbstclient attr monitors."$1".geometry)
+    x_coord=${geo#*+}
+    y_coord=${x_coord#*+}
+    x_coord=${x_coord%+*}
+    if [ "$x_coord" -lt "1920" ] ; then
+        # no panel on hoth on left monitor
+        exit 0
+    else
+        exec ~/git/barpyrus/barpyrus.py "$@"
+    fi
 else
     exec ~/git/barpyrus/barpyrus.py "$@"
 fi
