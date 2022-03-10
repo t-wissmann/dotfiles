774e892e87e6030d2418aa278046eb246c551c51
Thorsten Wißmann <edu@thorsten-wissmann.de>
2022-03-10 16:17:18 +0100

    Add docking script

---
+++ config/herbstluftwm/x1-docking-station.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+::() {
+    echo -e "\e[1;33m:: \e[0;32m$*\e[0m" >&2
+    "$@"
+}
+
+
+if :: lsusb |grep "043e:9a39 LG Electronics USA, Inc. USB Controls" > /dev/null ; then
+    # get last word of last line
+    output=$(xrandr | grep -v primary | grep ' connected'| cut -d' ' -f1)
+    :: xrandr --output eDP1 --auto --pos 0x0 --primary \
+        --output "$output" --off
+    sleep 1
+    :: xrandr --output eDP1 --auto --pos 0x0 --primary \
+        --output "$output" --pos 0x0 --auto
+    resolution=$(xrandr --listmonitors | grep "$output" | sed 's,/[0-9]\+,,g' | grep -oE '[0-9]+x[0-9]+\+0\+0' | tail -n 1)
+    :: herbstclient set_monitors "$resolution"
+    :: herbstclient reload
+else
+    herbstclient detect_monitors
+    herbstclient reload
+fi
+
+
