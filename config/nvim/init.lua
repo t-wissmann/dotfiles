55b4149db2d84b228f6f0301dea7e1bdad8555e0
Thorsten Wißmann <edu@thorsten-wissmann.de>
2023-03-30 11:25:16 +0200

    Remove nvim-cmp which breaks keyword completion

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -174,36 +174,36 @@ return require('packer').startup(function()
   -- use {
   --   'lervag/vimtex'
   -- }
-  use({'hrsh7th/nvim-cmp',
-    requires = { 'hrsh7th/cmp-vsnip' },
-    requires = { 'hrsh7th/vim-vsnip' },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-    end
-  })
+  -- use({'hrsh7th/nvim-cmp',
+  --   requires = { 'hrsh7th/cmp-vsnip' },
+  --   requires = { 'hrsh7th/vim-vsnip' },
+  --   config = function()
+  --     local cmp = require('cmp')
+  --     cmp.setup({
+  --       snippet = {
+  --         expand = function(args)
+  --           vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+  --         end,
+  --       },
+  --       window = {
+  --         completion = cmp.config.window.bordered(),
+  --         documentation = cmp.config.window.bordered(),
+  --       },
+  --       mapping = cmp.mapping.preset.insert({
+  --         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+  --         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+  --         ['<C-Space>'] = cmp.mapping.complete(),
+  --         ['<C-e>'] = cmp.mapping.abort(),
+  --         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --       }),
+  --       sources = cmp.config.sources({
+  --         { name = 'nvim_lsp' },
+  --       }, {
+  --         { name = 'buffer' },
+  --       })
+  --     })
+  --   end
+  -- })
   use({'morhetz/gruvbox',
     config = function()
       setup_colorscheme()
@@ -214,7 +214,7 @@ return require('packer').startup(function()
     end
   })
   use({'neovim/nvim-lspconfig',
-      requires = { 'hrsh7th/cmp-nvim-lsp' },
+      -- requires = { 'hrsh7th/cmp-nvim-lsp' },
       config = function()
         require('lspconfig').texlab.setup({
             on_attach = on_attach,
