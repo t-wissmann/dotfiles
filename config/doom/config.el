28b6cb2f63e2bb3478a3a9dc00003e8889a90adf
Thorsten Wißmann <edu@thorsten-wissmann.de>
2023-10-02 11:22:42 +0200

    vim options

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,4 +1,5 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; vim: ts=2 sw=2
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
