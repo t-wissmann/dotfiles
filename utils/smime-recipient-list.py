64973703ed6798c099d191183209126ab52e3731
Thorsten Wißmann <edu@thorsten-wissmann.de>
2024-10-13 10:52:09 +0200

    Fix parsing of hex serial numbers and add verbose msg

--- utils/smime-recipient-list.py
+++ utils/smime-recipient-list.py
@@ -12,6 +12,11 @@ import subprocess
 import sys
 import textwrap
 
+
+def debug(msg):
+    print(f':: {msg}', file=sys.stderr)
+
+
 class Openssl:
     def __init__(self, openssl_command):
         self.openssl_command = openssl_command
@@ -19,6 +24,7 @@ class Openssl:
     def get_certificate_serial_number(self, certificate_file):
         """Given a certificate_file filepath, return its serial number as an int"""
         command = [self.openssl_command, 'x509', '-in', certificate_file, '-serial', '-noout']
+        # debug(f'running {command}')
         proc = subprocess.run(command, stdout=subprocess.PIPE)
         # output should be of the form 'serial=HEXADECIMALNUMBER'
         try:
@@ -30,6 +36,7 @@ class Openssl:
         """run smime -pk7out, return its output"""
         command = [self.openssl_command, 'smime', '-pk7out']
         command += ['-inform', 'DER', '-in', encrypted_file]
+        debug(f'running: {command}')
         proc = subprocess.run(command, stdout=subprocess.PIPE)
         return proc.stdout.decode()
 
@@ -37,8 +44,8 @@ class Openssl:
         """extract all serial numbers via openssl pkcs7 -noout -print"""
         command = [self.openssl_command, 'pkcs7', '-noout', '-print']
         proc = subprocess.run(command, stdout=subprocess.PIPE, text=True, input=pk7buf)
-        for match in re.finditer('serial: ([0-9]+)', proc.stdout):
-            yield int(match.group(1))
+        for match in re.finditer(r'serial: (0x[0-9a-fA-F]+|[0-9]+)(\s|$)', proc.stdout):
+            yield int(match.group(1), 0)  # base = 0 --> autodetect
 
     def list_recipient_serial_numbers(self, encrypted_file):
         """Do essentially:
@@ -92,6 +99,8 @@ def main():
     parser.add_argument('--print-path', action='store_true',
                         help='print path of recipient certificates')
     parser.add_argument('--private-keys', nargs='*', default=[], help='private keys for decrypt')
+    parser.add_argument('--try-only-private', default=False, action='store_true',
+                        help='only read certificate files with a matching private key')
     parser.add_argument('--decrypt', action='store_true',
                         help='decrypt using one of the private keys passed.\
                               the key must have the same file name as the certificate.')
@@ -100,10 +109,17 @@ def main():
     args = parser.parse_args()
     openssl = Openssl(args.openssl)
 
+
+    private_keys = {}
+    for filepath in args.private_keys:
+        private_keys[os.path.basename(filepath)] = filepath
+
     # get the serial number of every smime-certfile:
     serialnum2cert = {}
     for i in args.certificates:
-        serialnum2cert[openssl.get_certificate_serial_number(i)] = i
+        if not args.try_only_private or os.path.basename(i) in private_keys:
+            debug(f'reading serial number of {i}')
+            serialnum2cert[openssl.get_certificate_serial_number(i)] = i
     if args.list_serials:
         for serialnum, keyfile in serialnum2cert.items():
             print("{} --> {}".format(keyfile, serialnum))
@@ -111,19 +127,18 @@ def main():
     if args.print_path or args.decrypt:
         matching_keys = []
         for i in recipients:
+            #debug(f'mail encrypted for: {i}')
             if i in serialnum2cert:
                 matching_keys.append(serialnum2cert[i])
     if args.print_path:
         for i in matching_keys:
             print(i)
     if args.decrypt:
-        private_keys = {}
-        for filepath in args.private_keys:
-            private_keys[os.path.basename(filepath)] = filepath
         key_found = None
         for fp in matching_keys:
             if os.path.basename(fp) in private_keys:
                 priv_key_path = private_keys[os.path.basename(fp)]
+                debug(f'Decryption via key {priv_key_path}')
                 # print("We can use {} and {}".format(priv_key_path, fp))
                 key_found = (priv_key_path, fp)
         if key_found is None:
