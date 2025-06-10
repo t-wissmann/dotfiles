035b06748fd4b9aec32f401713aa6b17a164dab5
Thorsten Wißmann <edu@thorsten-wissmann.de>
2025-06-10 16:03:26 +0200

    temporary hack

--- config/jgmenu/menu.py
+++ config/jgmenu/menu.py
@@ -296,7 +296,9 @@ def main():
               Sep(),
               Item('Shutdown', 'gnome-shutdown', 'systemctl poweroff -i'),
            ])),
-      Item('Suspend', 'gnome-logout', 'systemctl suspend -i'),
+      Item('Suspend', 'gnome-logout',
+           ('killall qutebrowser ; ' if socket.gethostname() in ['hoth'] else '')
+           + 'systemctl suspend -i'),
     ])
 
 
